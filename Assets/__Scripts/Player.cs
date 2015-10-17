51f1b6e2617f8709e40ea9409ed18dc77e1e3d8a
Reece LaFortune <ReeceLaFortune@gmail.com>
2015-10-16 22:25:03 -0400

    Scene 0 and Player.cs

---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
