d6471b810f3dc7342d0c6a51b40d7fc480dcc27a
Reece LaFortune <ReeceLaFortune@gmail.com>
2015-10-17 16:50:22 -0400

    BotPlayer recommit

---
+++ Assets/__Scripts/BotPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BotPlayer : Player {
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        upKey = KeyCode.W;
+        downKey = KeyCode.S;
+        leftKey = KeyCode.A;
+        rightKey = KeyCode.D;
+    }
+}
