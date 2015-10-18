cffcabab86569d8f6b106d1a1d7565232f9d02f0
dbraunstein <dbraunst@umich.edu>
2015-10-18 09:12:48 -0400

    adding files so I can pull

---
+++ Assets/__Scripts/Opacity.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Opacity : MonoBehaviour {
+
+    void Start()
+    {
+         
+    }
+    
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject obj = col.gameObject;
+        if (col.tag == "Board")
+        {
+            MeshRenderersEnable(obj, true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        GameObject obj = col.gameObject;
+        if (col.tag == "Board")
+        {
+            MeshRenderersEnable(obj, false);
+        }
+    }
+
+    void MeshRenderersEnable(GameObject objs, bool enable)
+    {
+        if (objs.transform.childCount > 0)
+        {
+            for(int i = 0; i < objs.transform.childCount; ++i)
+            {
+                objs.transform.GetChild(i).gameObject.SetActive(enable);
+            }
+        }
+        else
+        {
+            objs.SetActive(enable);
+        }
+    }
+}
