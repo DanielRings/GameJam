8e7579b0ce7187b63e0200a36d3857a57ebc811a
Tomas <tomamed@umich.edu>
2015-10-18 18:09:43 -0400

    Looping Game

---
+++ Assets/__Scripts/LivesGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LivesGUI : MonoBehaviour {
+	public static LivesGUI S;
+
+	void Awake()
+	{
+		S = this;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
