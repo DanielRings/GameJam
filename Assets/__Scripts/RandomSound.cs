00a779686f9dca66eb176ef7532c61541bacf4d4
dbraunstein <dbraunst@umich.edu>
2015-10-18 02:24:19 -0400

    added bird squawk sounds (3) and a script to be added to the bomb that plays one of the three files at random upon instantiation. (RandomSound)

---
+++ Assets/__Scripts/RandomSound.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class RandomSound : MonoBehaviour {
+	private int number = 0;
+	public AudioSource sound1;
+	public AudioSource sound2;
+	public AudioSource sound3;
+
+	// Use this for initialization
+	void Start () {
+		number = Random.Range (1, 3);
+		if (number == 1) {
+			AudioSource sound1 = GetComponent<AudioSource> ();
+			sound1.Play ();
+		} else if (number == 2) {
+			AudioSource sound2 = GetComponent<AudioSource> ();
+			sound2.Play ();
+		}
+		if (number == 3) {
+			AudioSource sound3 = GetComponent<AudioSource> ();
+			sound3.Play ();
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
