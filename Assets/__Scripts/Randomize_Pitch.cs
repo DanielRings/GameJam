92d2d0c7ae038c22c9a9556c80ec5d0ee1a62e4f
dbraunstein <dbraunst@umich.edu>
2015-10-18 00:19:36 -0400

    created a prefab for step random (idk why), also made a script to randomize pitch playback of audio and also sound effect to sandstep

---
+++ Assets/__Scripts/Randomize_Pitch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+ public class Randomize_Pitch : MonoBehaviour {
+//	public float initDelay = 0.0;
+//	public float between = 0.0;
+	public float footAudioVol = 1.0f;
+	public float volumeMod = 0.05f;
+	public float pitchMod = 0.05f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 velocity = gameObject.GetComponentInParent<Rigidbody> ().velocity;
+		if (velocity.x != 0 && velocity.z != 0) 
+		{
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.pitch = Random.Range(1.0f - pitchMod, 1.0f + pitchMod);
+			audio.volume = Random.Range(footAudioVol - volumeMod, footAudioVol + volumeMod);
+			if(!audio.isPlaying)
+			{
+				audio.Play();
+				audio.Play(44100);
+			}
+		}
+	}
+}
