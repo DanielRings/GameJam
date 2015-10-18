5e8b6f01ab71cb40dae4542a5c7bb550e5e8480e
Reece LaFortune <ReeceLaFortune@gmail.com>
2015-10-18 06:34:50 -0400

    Added opacity altering script

---
+++ Assets/__Scripts/Opacity.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Opacity : MonoBehaviour {
+    
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject obj = col.gameObject;
+        if (col.tag == "Board")
+        {
+            MeshRenderersEnable(obj, true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        GameObject obj = col.gameObject;
+        if (col.tag == "Board")
+        {
+            MeshRenderersEnable(obj, false);
+        }
+    }
+
+    void MeshRenderersEnable(GameObject objs, bool enable)
+    {
+        if (objs.transform.childCount > 0)
+        {
+            foreach(var meshRenderer in objs.transform.GetComponentsInChildren<MeshRenderer>())
+            {
+                meshRenderer.enabled = enable;
+            }
+        }
+        else
+        {
+            objs.GetComponent<MeshRenderer>().enabled = enable;
+        }
+    }
+}
