9b484e07a1f1b3564a9a2e069eaf2c6c5f837438
dbraunstein <dbraunst@umich.edu>
2015-10-18 03:01:29 -0400

    added script for bird squawk

--- Assets/__Scripts/RandomSound.cs
+++ Assets/__Scripts/RandomSound.cs
@@ -1,27 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
-
 public class RandomSound : MonoBehaviour {
 	private int number = 0;
-	public AudioSource sound1;
-	public AudioSource sound2;
-	public AudioSource sound3;
+	public List <AudioClip> birds; 
+	//AudioSource[] birds = new AudioSource[3];
 
 	// Use this for initialization
 	void Start () {
-		number = Random.Range (1, 3);
-		if (number == 1) {
-			AudioSource sound1 = GetComponent<AudioSource> ();
-			sound1.Play ();
-		} else if (number == 2) {
-			AudioSource sound2 = GetComponent<AudioSource> ();
-			sound2.Play ();
-		}
-		if (number == 3) {
-			AudioSource sound3 = GetComponent<AudioSource> ();
-			sound3.Play ();
+		number = Random.Range (0, 3);
+		AudioSource source = GetComponent<AudioSource> ();
+		source.clip = birds [number];
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (!source.isPlaying) {
+				source.Play ();
+			}
 		}
 	}
 
