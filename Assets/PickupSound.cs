d7b2563c05195adfb2208e33885bae863a93ebd4
Tomas <tomamed@umich.edu>
2015-10-18 17:55:59 -0400

    Seagull Pickup SOund

---
+++ Assets/PickupSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class PickupSound : MonoBehaviour {
+	public static PickupSound S;
+
+	public float footAudioVol = 1.0f;
+	public AudioClip audio2;
+	public AudioSource audio;
+	
+
+	void Awake()
+	{
+		S = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		audio = GetComponent<AudioSource>();
+		audio.clip = audio2;
+	}
+
+}
