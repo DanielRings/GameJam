56f830946ed0f9f6f91605b6e4bddc43e114e67e
Daniel Rings <rings.daniel@gmail.com>
2015-10-17 22:55:14 -0400

    added camera follow to each player

---
+++ Assets/__Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+
+    void LateUpdate()
+    {
+        this.transform.position = new Vector3(target.transform.position.x + xOffset,
+                                              target.transform.position.y + yOffset,
+                                              target.transform.position.z + zOffset);
+    }
+}
