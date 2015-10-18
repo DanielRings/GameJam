040f9e18b64a7ec2faef299bfa891cc9104145a6
Daniel Rings <rings.daniel@gmail.com>
2015-10-17 23:55:34 -0400

    lerp camera

--- Assets/__Scripts/CameraFollow.cs
+++ Assets/__Scripts/CameraFollow.cs
@@ -3,14 +3,16 @@
 public class CameraFollow : MonoBehaviour {
 
     public GameObject target;
+    public float smooth = 3.0f;
     public float xOffset;
     public float yOffset;
     public float zOffset;
 
     void LateUpdate()
     {
-        this.transform.position = new Vector3(target.transform.position.x + xOffset,
+        Vector3 newPos = new Vector3(target.transform.position.x + xOffset,
                                               target.transform.position.y + yOffset,
                                               target.transform.position.z + zOffset);
+        this.transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
     }
 }
